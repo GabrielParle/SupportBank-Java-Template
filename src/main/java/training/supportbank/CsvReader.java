5dd84ceec055478984048e2056bf782c211fff56
gparle <gabriel.j.parle@aexp.com>
2022-09-14 15:21:06 +0100

    add

--- src/main/java/training/supportbank/CsvReader.java
+++ src/main/java/training/supportbank/CsvReader.java
@@ -1,15 +1,28 @@
 package training.supportbank;
+
 import com.opencsv.CSVReader;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 
-
 public class CsvReader {
-//test
-    public void test(){
-        CSVReader reader = new CSVReader(new FileReader("emps.csv"), ',');
+    //test
+    public void test() throws IOException {
+        CSVReader reader = new CSVReader(new FileReader("src/main/Transactions2014.csv"), ',');
+
+        String[] nextLine;
+//reads one line at a time
+        while ((nextLine = reader.readNext()) != null) {
+            for (String token : nextLine) {
+                System.out.print(token);
+            }
+            System.out.print("\n");
+        }
     }
+
+
+
 
 }
--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,14 @@
 package training.supportbank;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         // Your code here!
+
+
+        CsvReader csv = new CsvReader();
+        csv.test();
         System.out.println("Test!");
     }
 }
