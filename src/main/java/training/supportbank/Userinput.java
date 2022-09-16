1080aaf12a66601fc4c2db9812edbe7b1e73ddb8
gparle <gabriel.j.parle@aexp.com>
2022-09-16 11:43:49 +0100

    part one working with runagain

--- src/main/java/training/supportbank/Userinput.java
+++ src/main/java/training/supportbank/Userinput.java
@@ -4,11 +4,15 @@ import java.text.ParseException;
 import java.util.*;
 
 public class Userinput {
+
+    private Scanner sc = new Scanner(System.in);
+    public boolean running = true;
     public boolean requestUserInput () throws IOException, ParseException {
         CsvReader csv = new CsvReader();
         csv.test();
-        boolean running = true;
-            Scanner sc = new Scanner(System.in); //System.in is a standard input stream
+
+
+             //System.in is a standard input stream
             System.out.print("Do you want to (A)List All names of people or (B)List all transactions - Press a or b:");
             //reads string
             String str = sc.nextLine();
@@ -16,14 +20,29 @@ public class Userinput {
             ArrayList<User> allUsers =  csv.getAllUsers();
             //if user types a in console system to display all names
             if (str.equals("a")) {
-                running = false;
+
                 CalcualateBalances.calcBalance(allTransactions, allUsers);
+                runAgain();
             } else if (str.equals("b")) {
                 System.out.println("Please enter name of User?");
                 String nameLookUp = sc.nextLine();
                 ListAccount.listingPrint(allTransactions, allUsers, nameLookUp);
-                running = false;
+                runAgain();
             }
             return running;
+
+
+    }
+
+    public void runAgain(){
+        System.out.println("do you want to run another command y or n?");
+        String yesNo = sc.nextLine();
+        if(yesNo.equals("y") ){
+            running = true;
+        } else {
+            running = false;
+        }
+
+
     }
 }
