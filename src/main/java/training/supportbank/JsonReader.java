1a82e8995cd6acc96f4cd9e7caef1ed0ae38ca21
gparle <gabriel.j.parle@aexp.com>
2022-09-16 15:10:38 +0100

    refactor for json part way thorugh

---
+++ src/main/java/training/supportbank/JsonReader.java
@@ -0,0 +1,13 @@
+package training.supportbank;
+import com.google.code.gson;
+import com.google.gson.GsonBuilder;
+
+import java.time.LocalDate;
+
+public class JsonReader {
+//    GsonBuilder gsonBuilder = new GsonBuilder();
+//    gsonBuilder.registerTypeAdapter(LocalDate .class,(JsonDeserializer<LocalDate>)(jsonElement,type,jsonDeserializationContext)->
+//            // Convert jsonElement to a LocalDate here...
+//            );
+//    Gson gson = gsonBuilder.create();
+}
