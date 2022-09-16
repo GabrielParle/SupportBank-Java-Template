20462e98968dbead64085a49fb28054205c21c09
Philip Jordan Buuza <philipbuuza@gmail.com>
2022-09-16 14:15:56 +0100

    removed logging file from project

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,19 +1,24 @@
 package training.supportbank;
-
+//import org.apache.logging.log4j.LogManager;
+//import static org.apache.logging.log4j.LogManager.*;
+//import org.apache.logging.log4j.Logger;
 import java.io.IOException;
-
 import java.text.ParseException;
-
 import java.util.Scanner;
 import java.util.*;
 
+//import java.util.logging.Logger;
+//import java.util.logging.LogManager;
+
 public class Main {
-    private static final Logger LOGGER = LogManager.getLogger();
+
+    //private static final Logger LOGGER = LogManager.getLogger();
     public static void main(String args[]) throws IOException, ParseException {
         // Your code here!
 
-
+      //  LOGGER.info("eroor report");
         Userinput usi = new Userinput();
+        //LOGGER.debug("try this");
         //while loop for user inout request
         boolean running = true;
         while(running) {
